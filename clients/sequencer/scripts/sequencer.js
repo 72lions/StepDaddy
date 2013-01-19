f72a21890109ba13119d489213bd3a066b54b208
Tomek Augustyn <blog2t@gmail.com>
2013-01-19 17:39:56 +0100

    Sequencer WIP.

--- clients/sequencer/scripts/sequencer.js
+++ clients/sequencer/scripts/sequencer.js
@@ -6,12 +6,12 @@
     var _instruments = {};
     var _tracks = {};
 
-    var intruments = [
+    var instrumentsConfig = [
     {
         name: 'HiHat',
         tracks: [
             {
-                name: 'HiHat'
+                name: 'HiHat',
                 sampleURL: '909 HHCL 1.wav'
             }        
         ]
@@ -19,7 +19,7 @@
         name: 'Kick',
         tracks: [
             {
-                name: 'Kick'
+                name: 'Kick',
                 sampleURL: '909 KIK1.wav'
             }        
         ]
@@ -27,7 +27,7 @@
         name: 'Tom Hi',
         tracks: [
             {
-                name: 'Tom HI'
+                name: 'Tom HI',
                 sampleURL: '909 HI.TOM1.wav'
             }        
         ]
@@ -35,7 +35,7 @@
         name: 'Snare',
         tracks: [
             {
-                name: 'Snare'
+                name: 'Snare',
                 sampleURL: '909 SD1.wav'
             }        
         ]
@@ -43,24 +43,41 @@
         name: 'Tom Low',
         tracks: [
             {
-                name: 'Tom Low'
+                name: 'Tom Low',
                 sampleURL: '909 LOWTOM1.wav'
             }        
         ]
-    }
-]
+    }];
 
+    var samplesPath = '../common/resources/12-TR-909'; 
 
     this.initialize = function() {
       console.log('initialize Sequencer');
+      this.createInstruments();
     };
 
     this.createInstruments = function() {
+        _instruments = [];
+        for (var i = 0; i < instrumentsConfig.length; i++) {
+            var tracks = this.createTracks(i, instrumentsConfig[i].tracks);
+            var instrument = new mixr.models.Instrument(i, instrumentsConfig[i].name, tracks);
+            _instruments.push(instrument);
+        };
 
+        console.log('Instruments', _instruments);
     };
 
-    this.initialize();
+    this.createTracks = function(instrumentId, tracksConfig) {
+        var tracks = []; 
+        for (var i = 0; i < tracksConfig.length; i++) {
+            var track = new mixr.models.Track(instrumentId + ':' + i, tracksConfig[i]);
+            tracks.push(track);
+        };
 
+        return tracks;
+    }
+
+    this.initialize();
   };
 
 }());
